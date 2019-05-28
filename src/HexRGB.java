208fdbda40af5a7ba66b5103dc6d313599c349e2
Rodney McQuain <rodneymcquain95@gmail.com>
2019-05-27 20:31:58 -0400

    Removed # from color parsing format
    Adjusted private functions accordingly.

--- src/HexRGB.java
+++ src/HexRGB.java
@@ -8,7 +8,7 @@ public class HexRGB {
 	final int HEX_BASE = 16;
 	
 	public HexRGB(Color color) {
-		String hex = String.format("#%02X%02X%02X",
+		String hex = String.format("%02X%02X%02X",
 	            (int)(color.getRed() * 255),
 	            (int)(color.getGreen() * 255),
 	            (int)(color.getBlue() * 255 ))
@@ -20,14 +20,14 @@ public class HexRGB {
 	}
 	
 	private int getRedDigits(String hex) {
-		return Integer.parseInt(hex.substring(1, 3), HEX_BASE);
+		return Integer.parseInt(hex.substring(0, 2), HEX_BASE);
 	}
 	
 	private int getGreenDigits(String hex) {
-		return Integer.parseInt(hex.substring(3, 5), HEX_BASE);
+		return Integer.parseInt(hex.substring(2, 4), HEX_BASE);
 	}
 	
 	private int getBlueDigits(String hex) {
-		return Integer.parseInt(hex.substring(5, 7), HEX_BASE);
+		return Integer.parseInt(hex.substring(4, 6), HEX_BASE);
 	}
 }
