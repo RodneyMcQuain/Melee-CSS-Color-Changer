eaf33ae722106369dfbf12eba0c1c8d3b8a12201
Rodney McQuain <rodneymcquain95@gmail.com>
2019-06-01 12:56:10 -0400

    Initial Commit
    Just has static constants for string options.

---
+++ src/SelectBackgroundUtility.java
@@ -0,0 +1,13 @@
+
+public class SelectBackgroundUtility {
+	public final static String DO_NOTHING = "Do Nothing";
+	public final static String ONE_COLOR = "One Color";
+	public final static String ALTERNATE = "Alternate";
+	public final static String ALTERNATE_FULL = "Alternate Full";
+	public final static String TRI = "Tri";
+	public final static String RANDOM = "Random";
+	public final static String RANDOM_DIAMOND = "Random Diamond";
+	public final static String RANDOM_FULL = "Random Full";
+	public final static String TRANSPARENT = "Transparent";
+	public final static String VISIBLE = "Visible";
+}
