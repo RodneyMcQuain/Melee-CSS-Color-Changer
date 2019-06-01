ce13e9ebd78f44770eac452f35138aeaba0701c8
Rodney McQuain <rodneymcquain95@gmail.com>
2019-06-01 13:47:40 -0400

    Added writeByOption function

--- src/SelectBackgroundUtility.java
+++ src/SelectBackgroundUtility.java
@@ -10,4 +10,39 @@ public class SelectBackgroundUtility {
 	public final static String RANDOM_FULL = "Random Full";
 	public final static String TRANSPARENT = "Transparent";
 	public final static String VISIBLE = "Visible";
+	
+	public static void writeByOption(String filename, String option, Format4248[] selectBackgroundColors) {
+		HexRGB[] twoColorRgb1 = { selectBackgroundColors[0].primaryColor, selectBackgroundColors[0].secondaryColor };
+		HexRGB[] twoColorRgb2 = { selectBackgroundColors[1].primaryColor,  selectBackgroundColors[1].secondaryColor };
+		
+		switch (option) {
+			case SelectBackgroundUtility.RANDOM:
+				SelectBackground.writeRandom(filename);
+				break;
+			case SelectBackgroundUtility.RANDOM_DIAMOND:
+				SelectBackground.writeRandomDiamond(filename);
+				break;
+			case SelectBackgroundUtility.RANDOM_FULL:
+				SelectBackground.writeRandomFull(filename);
+				break;
+			case SelectBackgroundUtility.ONE_COLOR:
+				SelectBackground.writeOneColor(filename, twoColorRgb1);
+				break;
+			case SelectBackgroundUtility.TRANSPARENT:
+				SelectBackground.writeTransparent(filename);
+				break;
+			case SelectBackgroundUtility.VISIBLE:
+				SelectBackground.writeVisible(filename);
+				break;
+			case SelectBackgroundUtility.ALTERNATE:
+				SelectBackground.writeAlternate(filename, twoColorRgb1, twoColorRgb2);
+				break;
+			case SelectBackgroundUtility.ALTERNATE_FULL:
+				SelectBackground.writeAlternateFull(filename, twoColorRgb1, twoColorRgb2);
+				break;
+			case SelectBackgroundUtility.TRI:
+				SelectBackground.writeTri(filename, twoColorRgb1, twoColorRgb2);
+				break;
+		}
+	}
 }
